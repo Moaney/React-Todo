3e19702fb42bc2419ce6eecfc18f48b88da5795b
Moaney <ladonnamoney@gmail.com>
2019-06-18 19:59:39 -0400

    form is showing in app with add and clear completed buttons

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,15 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from 'react';
+
+class TodoList extends React.Component {
+    render() {
+        return (
+            <form>
+                <input placeholder='New To-Do' type='text' />
+                <button>Add To-Do</button>
+                <button>Clear Completed</button>
+            </form>
+        )
+    }
+}
+
+export default TodoList;
